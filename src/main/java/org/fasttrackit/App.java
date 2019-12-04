a481077545d71d15c1ce69185410362c93d9a826
Robert Gal <robyka2009@gmail.com>
2019-12-04 21:16:12 +0200

    studied arrays and for loops

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -6,62 +6,60 @@ public class App {
         Game game = new Game();
         game.start();
 
-        //method implementation taken from object's class not from the variables class
-        //sub-classes can get super class as type
-        Vehicle joker = new JokerVehicle();
-        joker.setName("Joker");
-        joker.setFuelLevel(80);
-
-        //type casting
-        //variable's class determines what methods can be invoked
-        ((JokerVehicle) joker).fly();
-
-        joker.accelerate(60, 1);
-        System.out.println("Joker's total distance: " + joker.getTraveledDistance());
-
-        Car carWithDefaultEngine = new Car();
-
-        // git gud
-        System.out.println("Welcome to the Bestest Racing Game");
-
-        //create engine object
-        Engine carEngine = new Engine();
-        carEngine.manufacturer = "Renault";
-        carEngine.capacity = 2.2;
-
-        // instance variables
-        Car carReference = new Car(carEngine);
-        carReference.setName("Dacia");
-        carReference.setColor("blue");
-        carReference.setMileage(5);
-        carReference.setFuelLevel(100);
-        carReference.setMaxSpeed(255);
-        carReference.setRunning(false);
-        System.out.println(carReference.toString());
-
-        carReference.engine = carEngine;
-
-        System.out.println("Engine details... \n   " + carReference.engine.manufacturer + "\n   " + carReference.engine.capacity);
-
-        double accelerationDistance = carReference.accelerate(200, 1);
-        carReference.accelerate(120);
-        System.out.println("Acceleration distance: "+accelerationDistance);
-
-        Mechanic worstMechanicEver = new Mechanic();
-        worstMechanicEver.repairVehicle(carReference);
-        System.out.println("Total traveled distance after repair: " + carReference.getTraveledDistance() + "\n");
-
-
-        // concat
-        System.out.println("First Car Name = " + carReference.getName());
-
-        // second object
-        Car car2 = new Car(new Engine());
-        car2.engine.manufacturer = "Bavaria Motor Werk";
-        car2.engine.capacity = 3.2;
-        car2.setName("BMW");
-
-        System.out.println("Total vehicle count from Vehicle class: " + Vehicle.totalCount);
+//        //method implementation taken from object's class not from the variables class
+//        //sub-classes can get super class as type
+//        Vehicle joker = new JokerVehicle();
+//        joker.setName("Joker");
+//        joker.setFuelLevel(80);
+//
+//        //type casting
+//        //variable's class determines what methods can be invoked
+//        ((JokerVehicle) joker).fly();
+//
+//        joker.accelerate(60, 1);
+//        System.out.println("Joker's total distance: " + joker.getTraveledDistance());
+//
+//        Car carWithDefaultEngine = new Car();
+//
+//        // git gud
+//        //create engine object
+//        Engine carEngine = new Engine();
+//        carEngine.manufacturer = "Renault";
+//        carEngine.capacity = 2.2;
+//
+//        // instance variables
+//        Car carReference = new Car(carEngine);
+//        carReference.setName("Dacia");
+//        carReference.setColor("blue");
+//        carReference.setMileage(5);
+//        carReference.setFuelLevel(100);
+//        carReference.setMaxSpeed(255);
+//        carReference.setRunning(false);
+//        System.out.println(carReference.toString());
+//
+//        carReference.engine = carEngine;
+//
+//        System.out.println("Engine details... \n   " + carReference.engine.manufacturer + "\n   " + carReference.engine.capacity);
+//
+//        double accelerationDistance = carReference.accelerate(200, 1);
+//        carReference.accelerate(120);
+//        System.out.println("Acceleration distance: "+accelerationDistance);
+//
+//        Mechanic worstMechanicEver = new Mechanic();
+//        worstMechanicEver.repairVehicle(carReference);
+//        System.out.println("Total traveled distance after repair: " + carReference.getTraveledDistance() + "\n");
+//
+//
+//        // concat
+//        System.out.println("First Car Name = " + carReference.getName());
+//
+//        // second object
+//        Car car2 = new Car(new Engine());
+//        car2.engine.manufacturer = "Bavaria Motor Werk";
+//        car2.engine.capacity = 3.2;
+//        car2.setName("BMW");
+//
+//        System.out.println("Total vehicle count from Vehicle class: " + Vehicle.totalCount);
 
 
     }
--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -3,14 +3,51 @@ package org.fasttrackit;
 import java.util.Scanner;
 
 public class Game {
+
+
+    private Track[] tracks = new Track[3];
+
     public void start() {
         System.out.println("Starting game...");
+
+        initializeTracks();
+        displayTracks();
+
         String vehicleName = getVehicleNameFromUser();
-        System.out.println("Name from user: "+ vehicleName + "\n");
+        System.out.println("Name from user: " + vehicleName + "\n");
 
         int playersNumber = getPlayersNumberFromUser();
-        System.out.println("Players number: "+ playersNumber + "\n");
+        System.out.println("Players number: " + playersNumber + "\n");
 
+    }
+
+    private void initializeTracks() {
+        Track track1 = new Track();
+        track1.setName("Silverstone");
+        track1.setLength(4.2);
+
+        Track track2 = new Track();
+        track2.setName("Le Mans");
+        track2.setLength(200.6);
+
+        Track track3 = new Track();
+        track3.setName("Test Track");
+        track3.setLength(51.6);
+
+        tracks[0] = track1;
+        tracks[1] = track2;
+        tracks[2] = track3;
+    }
+
+    private void displayTracks() {
+        System.out.println("Available tracks:");
+
+        //classic for loop
+        for (int i = 0; i < tracks.length; i++) {
+            if (tracks[i] != null) {
+                System.out.println((i + 1) + ". " + tracks[i].getName() + " - " + tracks[i].getLength() + "km");
+            }
+        }
     }
 
     private String getVehicleNameFromUser() {
